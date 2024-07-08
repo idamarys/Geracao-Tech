f0168c41d0726bf95ada1629cb2a3d82617062b3
Dâmarys Santos <damarysdev@gmail.com>
2024-07-08 12:06:04 -0300

    treinando lógica

---
+++ introducao-javascript/treino-logica/manipulacao-string/lista.js
@@ -0,0 +1,30 @@
+// var lista = ['b', 'o', 'l', 'a', 's']
+
+// function inverterLista(arg) {
+//     if (Array.isArray(arg)) {
+//         let invertido = new Array()
+
+//         let esquerda = 0
+//         let direita = arg.length - 1
+
+//         while (direita > esquerda) {
+//             arg[direita] = arg[esquerda]
+//             arg[esquerda] = arg[direita]
+//             esquerda++
+//             direita--
+//         }
+
+//         // for (i = arg.length - 1; i >= 0; i--) {
+//         //     let temp = arg[esquerda]
+//         //     arg[arg.length -1] = arg[i]
+//         //     return arg
+//         // }
+
+//         return arg
+//     } else {
+//         return 'Função aceita apenas array como argumento'
+//     }
+// }
+
+// console.log(lista)
+// console.log(inverterLista(lista))
