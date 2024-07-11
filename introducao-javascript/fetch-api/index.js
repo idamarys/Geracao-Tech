5da42b04d0b201a06e1ba03c79e0e93a826eb1f6
Dâmarys Santos <damarysdev@gmail.com>
2024-07-11 11:30:40 -0300

    começando teste

---
+++ introducao-javascript/fetch-api/index.js
@@ -0,0 +1,6 @@
+const button  = document.getElementById('button')
+const output = document.getElementById('output')
+
+button.addEventListener('click', () => {
+    fetch('https://668feb37c0a7969efd9a22ef.mockapi.io/teste/product')
+})
