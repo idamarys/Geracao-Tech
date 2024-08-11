7519b223041b7bb4d49d28a79f72b55dbc509f5b
Dâmarys Santos <damarysdev@gmail.com>
2024-08-11 10:09:46 -0300

    iniciando header

---
+++ projeto-ecommerce/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.svg'
+
+export default function Header() {
+    return (
+        <>
+            <div>
+                <img src={logo} alt="logo da Digital Store em cor de rosa" />
+            </div>
+        </>
+    )
+}
--- projeto-ecommerce/src/main.jsx
+++ projeto-ecommerce/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <App />
   </StrictMode>,
 )
